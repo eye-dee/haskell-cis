5edda41abc535eb44b56e7faadd9600f1fbfcd8e
sausagerRoll <drozdovigor1996@gmail.com>
2020-01-03 10:31:10 +0100

    find local maximum

--- app/Main.hs
+++ app/Main.hs
@@ -12,3 +12,6 @@ main = do
     print (skips "hello!")
     print (skips [1])
     print (skips [True, False])
+    print (localMaxima [2,9,5,6,1])
+    print (localMaxima [2,3,4,1,5])
+    print (localMaxima [1,2,3,4,5])
--- src/Golf.hs
+++ src/Golf.hs
@@ -10,3 +10,11 @@ filterDividedBy x (y, _) = (mod y x) == 0
 skipsInternal :: [Int] -> [(Int, a)] -> [[a]]
 skipsInternal [] _ = []
 skipsInternal (i:is) (x) = (map snd (filter (filterDividedBy i) x)) : (skipsInternal is x)
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima [x] = []
+localMaxima [x,y] = []
+localMaxima (x:rest@(y:z:xs))
+    | x < y && y > z = y:localMaxima(rest)
+    | otherwise = localMaxima(rest)
