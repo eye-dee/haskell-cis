8bde2140bb3dcc55dab02c5bc4b2bc80c49c162c
sausagerRoll <drozdovigor1996@gmail.com>
2020-01-03 11:48:37 +0100

    frequency histogram

--- app/Main.hs
+++ app/Main.hs
@@ -15,3 +15,5 @@ main = do
     print (localMaxima [2,9,5,6,1])
     print (localMaxima [2,3,4,1,5])
     print (localMaxima [1,2,3,4,5])
+    putStr (histogram [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,5,5,5,5,5,5,5,5,5,5])
+--    putStr (histogram [1,4,5,4,6,6,3,4,2,4,9])
--- src/Golf.hs
+++ src/Golf.hs
@@ -1,5 +1,8 @@
 module Golf where
 
+import Data.List
+import Data.Char
+
 skips :: [a] -> [[a]]
 skips [] = []
 skips x = skipsInternal [1..length x] (zip [1..] x)
@@ -18,3 +21,27 @@ localMaxima [x,y] = []
 localMaxima (x:rest@(y:z:xs))
     | x < y && y > z = y:localMaxima(rest)
     | otherwise = localMaxima(rest)
+
+histogram :: [Integer] -> String
+histogram xs = let counts = map (length . (flip filter $ xs) . (==)) [0..9]
+                   height = foldl1 max counts
+                   lines = map (\x -> map (display . (>= x)) counts) [height, height - 1 .. 1]
+               in unlines $ lines ++ [replicate 10 '='] ++ [concatMap show [0..9]]
+
+display :: Bool -> Char
+display True = '*'
+display False = ' '
+--histogram :: [Int] -> String
+--histogram x = (intercalate "\n" (reverse (transpose (histogramToTable x)))) ++ "\n"
+--
+--histogramToTable :: [Int] -> [String]
+--histogramToTable x = histogramToTableInternal [0..9] x
+--
+--histogramToTableInternal :: [Int] -> [Int] -> [String]
+--histogramToTableInternal [] _ = []
+--histogramToTableInternal (x:xs) y = (histogramToTableWithIndex x y):(histogramToTableInternal xs y)
+--
+--histogramToTableWithIndex :: Int -> [Int] -> String
+--histogramToTableWithIndex i x =
+--    [intToDigit i] ++ "=" ++ (take (length (filter (i==) x)) (repeat (intToDigit i))) ++
+--    (take (10 - length (filter (i==) x)) (repeat ' '))
