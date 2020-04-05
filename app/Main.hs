c2f4be9e97e8fd11ae521ee6f79664dc06c09efc
sausagerRoll <drozdovigor1996@gmail.com>
2020-04-05 15:14:37 +0200

    add shopping

--- app/Main.hs
+++ app/Main.hs
@@ -29,7 +29,9 @@ execute pattern = do
     removeEach filtered
 
 main :: IO ()
-main = shopping1
+main = do
+    putStrLn $ foldl (++) ("") (items shopping1)
+    putStrLn $ show (total shopping1)
 
 main1 :: IO ()
 main1 = do
@@ -53,6 +55,14 @@ instance (Monoid w) => Monad (Writer w) where
         (y, v) = runWriter $ k x
     in Writer (y, u `mappend` v)
 
+data MyMonoid = MyMonoid (Integer, [String])
+
+instance Semigroup MyMonoid where
+    MyMonoid (x1, y1) <> MyMonoid (x2, y2) = MyMonoid(x1 + x2, y1 ++ y2)
+
+instance Monoid MyMonoid where
+    mempty = MyMonoid (0, [])
+    mappend (MyMonoid (x1, y1)) (MyMonoid (x2, y2)) = MyMonoid (x1 + x2, y1 ++ y2)
 
 instance (Monoid w) => Applicative (Writer w) where
   pure  = return
@@ -61,7 +71,7 @@ instance (Monoid w) => Applicative (Writer w) where
 instance (Monoid w) => Functor (Writer w) where
   fmap = liftM
 
-type Shopping = Writer (Sum Integer) ()
+type Shopping = Writer MyMonoid ()
 
 shopping1 :: Shopping
 shopping1 = do
@@ -70,7 +80,10 @@ shopping1 = do
   purchase "Lettuce"   328
 
 purchase :: String -> Integer -> Shopping
-purchase _ cost = writer ((), Sum cost)
+purchase item cost = writer ((), MyMonoid (cost, [item]))
 
 total :: Shopping -> Integer
-total s = let (Sum a) = execWriter s in a
+total x = let (MyMonoid (t, _)) = execWriter x in t
+
+items :: Shopping -> [String]
+items x = let (MyMonoid (_, l)) = execWriter x in l
